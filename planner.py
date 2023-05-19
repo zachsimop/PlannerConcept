4b97c07ffbdea48fdb3c347aec7f20a73eadc5e6
zach_simop <zachsimop@gmail.com>
2023-05-19 18:04:11 -0500

    sketches

--- planner.py
+++ planner.py
@@ -1,3 +1,5 @@
+import heapq
+
 class State:
     def __init__(self, dbase: dict[str, bool]) -> None:
         self.dbase = {var: val for var,val in dbase.items()}
@@ -32,21 +34,129 @@ class Operation:
     def apply(self, x: State) -> State:
         new_state = State(x.dbase)
         for var, val in self.eff.items():
-            new_state[var] = val
+            new_state.dbase[var] = val
+        return new_state
+
+class PriorityQueue:
+    def __init__(self):
+        self.heap =  list()
+    
+    def empty(self) -> bool:
+        return not self.heap
+    
+    def put(self, item: State, priority: int):
+        heapq.heappush(self.heap, [priority, item])
+    
+    def get(self) -> State:
+        return heapq.heappop(self.heap)[1]
 
 class Planner:
     def __init__(self, pddl_file : str = '') -> None:
-        self.ops = convert_pddl_file(pddl_file)
+        self.ops = {}
         pass
+
+    def calc_h(self, x: State, y: State) -> int:
+        '''
+            calculates the hamming distance of two lists
+            it assumes that the lists are not in order. 
+        '''
+        return self.calc_h_helper(x,y) + self.calc_h_helper(y,x)
+
+    def calc_h_helper(self, x: State, y: State) -> int:
+        '''
+            returns the number of propositions that are not
+            the same between two given states. It counts a difference
+            if the proposition in x is either not in y or labled as false in y.
+            It will not evaulate a proposition of x if labled as false.
+        '''
+        distance = 0
+        for var, val in x.dbase.items():
+            if val:
+                if var not in y.dbase or not y.dbase[var]:
+                    distance = distance + 1
+        return distance
 
     def make_plan_astar(self, milestones : list[State]) -> list[Operation]:
-        pass
+        '''
+            Add the initial state to the opened list
+            While open is not empty
+                state_analyzed = first in open.
+                if state is final_state, return state.
+        '''
+        #initalize
+        start = milestones[0]
+        goal = milestones[-1]
+        g = 0
+        h = self.calc_h(start, goal)
+        open_states = PriorityQueue()
+        open_states.put(start, h)
+        closed_states = list()
+        visited = dict[State , State]
+        
+        while not open_states.empty():
+            current = open_states.get()
+            if all(var in current.dbase and current.dbase[var] == val for var, val in goal.dbase.items()):
+                return self.getPath(visited, goal, start)
+            
+            closed_states.append(current)
+            for name,op in self.ops.items():
+                if op.check(current):
+                    print("applying" , name)
+                    new_state = op.apply(current)
+                    if new_state not in closed_states and new_state not in open_states.heap:
+                        g = g + 1
+                        if name == 'moveToR2': g = g + 1
+                        h = self.calc_h(new_state, goal)
+                        open_states.put(new_state, g + h)
+                        visited.update({new_state : current})
+        return []
 
-    def convert_pddl_file(pddl_file : str = '') -> list[Operation]:
+    def getPath(self, visited: dict[State:State], goal:State, start:State):
         '''
-            Convert the pddl file to Operations class
+            generates for a given state-to-state dictionary after A* is run
         '''
-        pass
+        current = goal
+        path = list()
+        while not all(var in current.dbase and current.dbase[var] == val for var, val in start.dbase.items()):
+            path.append(current)
+            current = visited[current]
+        path.append(current)
+        return path
+    
+    def init_test(self):
+        
+        #TEST OPS DEFINITION
+        #cleanRl
+        self.ops.update({'cleanR1':
+                         Operation(
+                             #precondictions
+                             {'inR1' : True, 'R1Clean': False},
+                             #effects
+                             {'R1Clean' : True})
+                         })
+
+        #cleanR2
+        self.ops.update({'cleanR2':
+                         Operation(
+                             {'inR2' : True, 'R2Clean': False},
+                             {'R2Clean' : True})
+                         })
+
+        #MoveR1
+        self.ops.update({'moveToR1':
+                         Operation(
+                             {'inR2' : True},
+                             {'inR2' : False, 'inR1' : True})
+                         })
+        
+
+        #MoveR2
+        self.ops.update({'moveToR2':
+                             Operation(
+                                 {'inR1' : True},
+                                 {'inR1' : False, 'inR2' : True})
+                         })
+        #self.ops.append()
     
 if __name__ == '__main__':
     '''
@@ -54,14 +164,67 @@ if __name__ == '__main__':
     '''
     x = State({'p': True, 'q': False})
     y = State({'p': True, 'q': True})
+    P = Planner()
     op1 = Operation({'p': True, 'q': False, 'r': False}, dict())
     op2 = Operation({'q': False}, dict())
 
     assert op1.check(x) == True
     assert op1.check(y) == False
 
-    operation-dev
+    # operation-dev
     assert op1.check_strict(x) == False
     assert op1.check_strict(y) == False
     assert op2.check_strict(x) == True
     assert op2.check_strict(y) == False
+
+    #heuristic-dev
+    '''      
+    S1 {p}
+    S2 {p, q}
+    '''
+    assert P.calc_h(x,y) == 1
+
+    '''
+    S1 {p, x}
+    S2 {p, q}
+    '''
+    x.dbase.update({'r': True})
+    assert P.calc_h(x,y) == 2
+
+    '''
+    Ensures that the function will handle the case where even if the state is strictly
+    represented, it will not count the value
+    S1 {p, x}
+    S2 {p, q}
+    '''
+    y.dbase.update({'s': False})
+    assert P.calc_h(x,y) == 2
+
+    '''
+    Ensures that if a state is represented as true in one state and false in the other,
+    it will be counted.
+    S1 {p, x, s}
+    S2 {p, q}
+    '''
+    x.dbase.update({'s': True})
+    assert P.calc_h(x,y) == 3
+
+    #A*-dev
+    P.init_test()
+
+    a = State({'inR1':True,'inR2':False,'R1Clean':False,'R2Clean':False})
+    b = State({'inR1':False,'inR2':True,'R1Clean':True,'R2Clean':True})
+    c = State({'inR1':True,'inR2':False,'R1Clean':False,'R2Clean':False})
+    sol = P.make_plan_astar([a,b])
+
+    #print out solution
+    print(*[var.dbase for var in sol], sep = "\n")
+'''
+                    if new_state in open_states:
+                        if new_cost <= new_state in open_states:
+                            open_states.remove(new_state)
+                    if new_state in closed:
+                        if new_cost <= new_state in closed:
+                            closed_states.'''
+
+    
