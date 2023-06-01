6ca85deeca80c8d3c1e526c9b97623ebe9caef65
KyleAMoore <kylito1@gmail.com>
2023-06-01 10:48:39 -0500

    Completed basic LLaMa in-context learning

---
+++ LLaMa/README.md
@@ -0,0 +1,1 @@
+Place your LLaMa.cpp installation, complete with model weights in this directory (should be in ./llama.cpp/)
--- pipelines.py
+++ pipelines.py
@@ -4,6 +4,7 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
 #Llama
+from langchain import FewShotPromptTemplate
 from langchain.embeddings import LlamaCppEmbeddings
 from langchain.llms import LlamaCpp
 from langchain.vectorstores import Chroma
@@ -11,11 +12,9 @@ from langchain.prompts.example_selector import SemanticSimilarityExampleSelector
 
 import os
 
+from plannerUtils import load_few_shot_examples
+
 def gen_openai_story(input_list : list[list[str]]):
-
-    #Get Key
-    os.environ["OPENAI_API_KEY"] = "" #ENTER YOUR API KEY HERE
-
     llm = OpenAI(model_name="text-davinci-003",temperature = 0.5, openai_api_key=os.getenv("OPENAI_API_KEY"))
     prompt = PromptTemplate(
         input_variables = ["genre", "subject", "details", "plan"],
@@ -42,56 +41,59 @@ def gen_openai_story(input_list : list[list[str]]):
     print(story_str)
     return stories
 
-def gen_llama_story(input_list):
-    
-    llm = LlamaCpp( model_path="LLAMA_MODEL_PATH", verbose=True)
+def gen_llama_story(input_list, genre='Fiction'):
+    llm = LlamaCpp( model_path="./LLaMa/llama.cpp/models/7B/ggml-model-q4_0.bin", verbose=True)
 
     #prompt to format user's perferences
     story_prompt = PromptTemplate(
-        input_variables = ["genre", "subject", "details", "plan"],
-        template = "You are a professional {genre} writer. I am a computer scientist. "   +
-        "We are collaborating on an experimental writing technique. I will provide you "  +
-        "with lists operations that result from a classical planner built on "            +
-        "propositional logic, each by a *. For example, move-11-21 is an operation that " +
-        "moves an agent from cell (1,1) to cell (2,1) in a grid. You will respond with "  +
-        "a fictional story about {subject} where main character takes actions that "      +
-        "math the plan operations. Ensure {details}. For example, \"move-11-21 * "        +
-        "move-21-31 * move-31-41 * move-41-42\" should give a story where the main "      +
-        "character is traveling. Here is your plan: {plan}"  
+        input_variables = ["genre"],
+        template = "{genre} short stories:"  
     )
+    story_prompt_text = story_prompt.format(genre=genre)
 
     #prompt to format few shot examples
-    example_prompt = PromptTemplate(input_variables=["prompt", "story"], template="{prompt}\n{story}")
+    example_prompt = PromptTemplate(input_variables=["prompt", "story"], template="{prompt}:\n{story}")
 
     #from our examples, create an example_selector so we can select
     #semantically relevant examples
     example_selector = SemanticSimilarityExampleSelector.from_examples(
         load_few_shot_examples('./domain_examples/few-shot-examples.json'),
-        LlamaCppEmbeddings(model_path="LLAMA_MODEL_PATH"),
+        LlamaCppEmbeddings(model_path="./LLaMa/llama.cpp/models/7B/ggml-model-q4_0.bin"),
         Chroma,
-        k=1
+        k=3
     )
 
-    #We go through each input to individually select semantically relevant examples
-    for var in input_list:
-
-        story_prompt_text = story_prompt.format(var)
-        
-        #request the semantically similar examples
-        selected_examples = example_selector.select_examples({"prompt": story_prompt_text})
-
+    # We go through each input to individually select semantically relevant examples
+    running_output = []
+    for op in input_list:
         #create a prompt that positions selected examples above the
         #user prompt template
+        suff_pre = ''.join([f'{x[0]}:\n{x[1]}\n\n' for x in running_output])
+        # map(lambda x: f'{x[0]}:\n{x[1]}\n\n', running_output)
+
         few_shot_prompt = FewShotPromptTemplate(
             example_selector=example_selector, 
-            example_prompt=example_prompt, 
+            example_prompt=example_prompt,
+            prefix=story_prompt_text,
+            suffix=suff_pre+'{input}:\n',
             input_variables=["input"]
         )
-        
-        llm_chain = LLMChain(llm=llm, prompt=few_shot_prompt)
 
         #for each of the user's inputs, apply the input to the
         #few_shot prompt. 
-        llm_chain.apply(story_prompt_text)
-        llm_chain.run()
-    
+        llm_chain = LLMChain(llm=llm, prompt=few_shot_prompt)
+        output = llm_chain({'input':op})
+        
+        #strip overgeneration from output
+        output_text = output['text']
+        output_text = output_text.split('\n\n')[0]
+
+        #add output to in-context learning set
+        running_output.append((op, output_text))
+
+    return running_output
+
+if __name__ == '__main__':
+    story = gen_llama_story(['move-12-11', 'move-11-21'], 'Romance')
+
+    print(story)
